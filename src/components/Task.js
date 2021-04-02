36d6219c1487c907ef9ccfbed9231f905d4ad43d
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-04-02 20:34:29 +0200

    init

---
+++ src/components/Task.js
@@ -0,0 +1,26 @@
+// For inputing the tasks
+
+import React, { useContext } from "react";
+import { TaskListContext } from "./TaskListContext";
+
+const Task = ({ task }) => {
+  const { removeTask, findItem } = useContext(TaskListContext);
+  return (
+    <li className="list-item">
+      <span>{task.title}</span>
+      <div>
+        <button
+          onClick={() => removeTask(task.id)}
+          className="btn-delete task-btn"
+        >
+          <i className="fas fa-trash-alt"></i>{" "}
+        </button>{" "}
+        <button className="btn-edit task-btn" onClick={() => findItem(task.id)}>
+          <i className="fas fa-pen"></i>{" "}
+        </button>
+      </div>
+    </li>
+  );
+};
+
+export default Task;
